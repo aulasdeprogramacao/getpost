7400c5d14aa748feb88fcde79b1aa8cde44e0a5e
aulasdeprogramacao <167897300+aulasdeprogramacao@users.noreply.github.com>
2024-05-02 14:28:49 -0300

    Add files via upload

---
+++ MostrarDadosGET.php
@@ -0,0 +1,5 @@
+<?php
+echo "O nome é: " . $_GET["f_nome"];
+echo "<br>O email é: " . $_GET["f_email"];
+echo "<br>A senha é: " . $_GET["f_senha"];
+?>
---
+++ MostrarDadosPOST.php
@@ -0,0 +1,5 @@
+<?php
+echo "O nome é: " . $_POST["f_nome"];
+echo "<br>O email é: " . $_POST["f_email"];
+echo "<br>A senha é: " . $_POST["f_senha"];
+?>
